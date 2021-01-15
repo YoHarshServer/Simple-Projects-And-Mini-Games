fb021b4eaaa2877154d4e6f274957ec6fef4a147
Harshjhadevelo <77202692+Harshjhadevelo@users.noreply.github.com>
2021-01-15 14:26:59 +0530

    Add files via upload

---
+++ DifferenceCALCULATOR.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+long long x, y;
+long long difference_d;
+cout << "Type A Number: ";
+cin >> x;
+cout << "Typer another Number: ";
+cin >> y;
+difference_d = x - y;
+cout << "Your Difference Is: " << difference_d;
+
+return 0;
+}
